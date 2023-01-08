fe62a0ad311c629d57c2e45991594d3cc9373faa
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2023-01-08 20:14:11 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,51 +1,50 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-const inputDelay = form.elements.delay;
-const inputStep = form.elements.step;
-const inputAmount = form.elements.amount;
-const btn = form.querySelector('button');
 
 form.addEventListener('submit', callback);
 
 function callback(evt) {
   evt.preventDefault();
-  const delayValue = Number(inputDelay.value);
-  const stepValue = Number(inputStep.value);
-  const amountValue = Number(inputAmount.value);
-
+  const delayValue = Number(evt.currentTarget.elements.delay.value); //я не зовсім зрозумів, чому такій запис краще за окремі константи
+  const stepValue = Number(evt.currentTarget.elements.step.value);
+  const amountValue = Number(evt.currentTarget.elements.amount.value);
   promiseGeneration(delayValue, stepValue, amountValue);
 }
 
 function promiseGeneration(delay, step, amount) {
-  btn.setAttribute('disabled', 'true');
+  form.querySelector('button').setAttribute('disabled', 'true');
   console.log('test');
   for (let i = 0; i < amount; i += 1) {
-    setTimeout(createPromise, delay, i, delay);
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      })
+      .finally(() => form.querySelector('button').removeAttribute('disabled'));
     delay += step;
   }
 }
 
+// та взагалі не розумію, чому цей варіант кращій за перший. Прошу пояснити
+
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  });
 
-  promise
-    .then(({ position, delay }) => {
-      Notiflix.Notify.success(
-        `✅ Fulfilled promise ${position + 1} in ${delay}ms`
-      );
-    })
-    .catch(({ position, delay }) => {
-      Notiflix.Notify.warning(
-        `❌ Rejected promise ${position + 1} in ${delay}ms`
-      );
-    })
-    .finally(() => btn.removeAttribute('disabled'));
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
