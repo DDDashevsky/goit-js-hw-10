d699789df3f89263e2daa6db9ff721500aab71bf
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2023-01-13 19:08:07 +0200

    v2

--- src/index.js
+++ src/index.js
@@ -18,28 +18,34 @@ refs.input.addEventListener('input', _.debounce(onInput, 300));
 
 function onInput() {
   if (refs.input.value.trim().length === 0) {
-    refs.list.innerHTML = '';
-    refs.info.innerHTML = '';
+    clearMarkup(refs.list);
+    clearMarkup(refs.info);
     return;
   }
 
   fetchCountry(refs.input.value.trim())
     .then(onSuccess)
-    .catch(error =>
+    .catch(() =>
       Notiflix.Notify.failure('Oops, there is no country with that name')
     );
 }
 
 function onSuccess(value) {
   if (value.length === 1) {
-    refs.list.innerHTML = '';
+    clearMarkup(refs.list);
     refs.info.innerHTML = markup.makeCard(value);
   } else if (value.length > 1 && value.length < 11) {
-    refs.info.innerHTML = '';
+    clearMarkup(refs.info);
     refs.list.innerHTML = markup.makeList(value);
   } else if (value.length > 10) {
+    clearMarkup(refs.list);
+    clearMarkup(refs.info);
     Notiflix.Notify.warning(
       'Too many matches found. Please enter a more specific name.'
     );
   }
+}
+
+function clearMarkup(item) {
+  item.innerHTML = '';
 }
